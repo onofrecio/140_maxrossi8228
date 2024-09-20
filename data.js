77c28afd1c595d755e5cff0023880a1d94ec0da7
onofrecio <alexlorman@hotmail.com>
2024-09-20 13:36:30 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-140_maxrossi8228_01_living",
+      "name": "140_maxrossi8228_01_Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31574385886839984,
+          "pitch": 0.4705301617446924,
+          "rotation": 0,
+          "target": "1-140_maxrossi8228_01_t"
+        },
+        {
+          "yaw": 1.6434772458120204,
+          "pitch": 0.3905797518079268,
+          "rotation": 0,
+          "target": "6-140_maxrossi8228_01_p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-140_maxrossi8228_01_t",
+      "name": "140_maxrossi8228_01_T",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0672218659141297,
+          "pitch": 0.5993457596398031,
+          "rotation": 0,
+          "target": "0-140_maxrossi8228_01_living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-140_maxrossi8228_01_bath2",
+      "name": "140_maxrossi8228_01_Bath2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7493606758270044,
+          "pitch": 0.5044541654530548,
+          "rotation": 0,
+          "target": "6-140_maxrossi8228_01_p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-140_maxrossi8228_01_bed1",
+      "name": "140_maxrossi8228_01_Bed1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0534458322939146,
+          "pitch": 0.5892592145773605,
+          "rotation": 0,
+          "target": "6-140_maxrossi8228_01_p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-140_maxrossi8228_01_bed2",
+      "name": "140_maxrossi8228_01_Bed2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6796819102432927,
+          "pitch": 0.5016302682662506,
+          "rotation": 0,
+          "target": "6-140_maxrossi8228_01_p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-140_maxrossi8228_01_bed3",
+      "name": "140_maxrossi8228_01_Bed3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.965185750039149,
+          "pitch": 0.5842034153859093,
+          "rotation": 0,
+          "target": "6-140_maxrossi8228_01_p"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-140_maxrossi8228_01_p",
+      "name": "140_maxrossi8228_01_P",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.04552508710578,
+          "pitch": 0.5892438772943329,
+          "rotation": 0,
+          "target": "0-140_maxrossi8228_01_living"
+        },
+        {
+          "yaw": 2.0419639475676634,
+          "pitch": 0.6454038281130217,
+          "rotation": 0,
+          "target": "7-140_maxrossi8228_01_bath1"
+        },
+        {
+          "yaw": 0.5340953363387388,
+          "pitch": 0.4862615133991479,
+          "rotation": 0,
+          "target": "3-140_maxrossi8228_01_bed1"
+        },
+        {
+          "yaw": -0.03260774368706798,
+          "pitch": 0.19802417072009248,
+          "rotation": 0,
+          "target": "2-140_maxrossi8228_01_bath2"
+        },
+        {
+          "yaw": -0.3935629285196107,
+          "pitch": 0.580448420908283,
+          "rotation": 0,
+          "target": "4-140_maxrossi8228_01_bed2"
+        },
+        {
+          "yaw": -0.2252210676608435,
+          "pitch": 0.3642886509504457,
+          "rotation": 0,
+          "target": "5-140_maxrossi8228_01_bed3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-140_maxrossi8228_01_bath1",
+      "name": "140_maxrossi8228_01_Bath1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.432682865602315,
+          "pitch": 0.6509702929052494,
+          "rotation": 0,
+          "target": "6-140_maxrossi8228_01_p"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
